4478489e61d5438c7ad91fc7da7b74928090ded6
ugvenkat <ugvenkat@hotmail.com>
2021-04-15 13:30:02 -0500

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAZUDYE43FNBNZGNFB"
-  secret_key = "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"
+  access_key = "XXXXX"
+  secret_key = "XXXXXX"
 }
 
 
